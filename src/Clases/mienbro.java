5380ea2e990e30f9787ce01127236b4303883223
Danii1314 <d.silva.andres73@hotmail.com>
2024-11-06 15:21:27 -0500

    cambio nombre

--- src/Clases/mienbro.java
+++ src/Clases/mienbro.java
@@ -6,7 +6,7 @@ public class mienbro extends Persona{
     @Override
     public void mostrarInformacion() {
         super.mostrarInformacion();
-        System.out.println("Nombre:"+getNombre()+"\n");
+        System.out.println("mencion:"+getNombre()+"\n");
         System.out.println("E-mail:"+getEmaill()+"\n");
         System.out.println("Edad:"+fechaNacimiento+"\n");
  }
