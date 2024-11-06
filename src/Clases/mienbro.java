e35ceb62218504026ea5e13b16eb8721189d7255
Danii1314 <d.silva.andres73@hotmail.com>
2024-11-05 21:16:43 -0500

    Añadir base
    Base del proyecto

---
+++ src/Clases/mienbro.java
@@ -0,0 +1,15 @@
+package Clases;
+import abstractos.Persona;
+
+public class mienbro extends Persona{
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Nombre:"+getNombre()+"\n");
+        System.out.println("E-mail:"+getEmaill()+"\n");
+        System.out.println("Edad:"+fechaNacimiento+"\n");
+ }
+    
+    
+}
---
+++ src/abstractos/Persona.java
@@ -0,0 +1,63 @@
+package abstractos;
+
+import java.time.LocalDate;
+
+public abstract class Persona {
+
+    protected String id;
+    protected String nombre;
+    protected String emaill;
+    protected LocalDate fechaNacimiento;
+   
+    public Persona() {
+    }
+
+    public Persona(String id, String nombre, String emaill, LocalDate fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.emaill = emaill;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmaill() {
+        return emaill;
+    }
+
+    public void setEmaill(String emaill) {
+        this.emaill = emaill;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [id=" + id + ", nombre=" + nombre + ", emaill=" + emaill + ", fechaNacimiento="
+                + fechaNacimiento + "]";
+    }
+
+    public void mostrarInformacion(){ }
+    
+
+}
